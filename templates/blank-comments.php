88e630607828f1214569d17f87a6101966a77653
caracoolnet <angel@caracool.net>
2026-08-05 11:12:28 +0200

    Create blank-comments.php template
    Add blank-comments.php template for disabled comments

---
+++ templates/blank-comments.php
@@ -0,0 +1,5 @@
+<?php
+// Silencio absoluto -- sustituye a comments.php cuando los comentarios estan
+// desactivados globalmente, para temas que no comprueban comments_open()
+// antes de pintar el formulario.
+if ( ! defined( 'ABSPATH' ) ) exit;
